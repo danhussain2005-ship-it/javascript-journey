7216ae5fc8b4f17483dce36a6c1c42d4e77454d2
Danish Hussain <danhussain2005@gmail.com>
2026-07-05 02:52:49 +0530

    Const, Var and Let

--- Basics.js
+++ Basics.js
@@ -1,5 +1,21 @@
 console.log("Hello World!");
 console.log("This is a basic JavaScript file.");
+const accountId = 123456
+var accountPassword = "DAN12345"
+let accountEmail = "Dan@gmail.com";
+accountCity = "Siliguri"
+let accountState 
 
-let myName = "John Doe";
-console.log(`Hey, I'm ${myName}!`);
+accountEmail = "Hussain@gmail.com"
+// accountId = "654321" // can't change a variable which we declare as const 
+accountCity = "Kolkata"
+accountPassword = "dan13245"
+
+/*
+Don't prefer using var, 
+because of issue in block scope and fuctional scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+
+//undefined = empty varaiable  

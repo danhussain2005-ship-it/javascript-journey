5fadcdb87078afeab314e15b467535795cbc9c2d
Danish Hussain <danhussain2005@gmail.com>
2026-07-06 21:26:28 +0530

    Dataypes

--- Basic.js
+++ Basic.js
@@ -1,21 +1,28 @@
-console.log("Hello World!");
+console.log("Hello!");
 console.log("This is a basic JavaScript file.");
+
+// Variables
 const accountId = 123456
 var accountPassword = "DAN12345"
 let accountEmail = "Dan@gmail.com";
 accountCity = "Siliguri"
 let accountState 
-
 accountEmail = "Hussain@gmail.com"
-// accountId = "654321" // can't change a variable which we declare as const 
+accountId = "654321" // can't change a variable which we declare as const 
 accountCity = "Kolkata"
 accountPassword = "dan13245"
-
 /*
 Don't prefer using var, 
 because of issue in block scope and fuctional scope 
 */
+//console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) //undefined = empty varaiable  
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
 
-//undefined = empty varaiable  
+//Datatypes
+let name = "Danish"
+let age = 20
+let isLoggedIn = true
+// Types of Data types:
+// (number = 2 to power 53), bigint, (string = ""), (null = standalone value), underfiend, (symbol = unique), object
+console.log(typeof null) // object
+console.log(typeof(name)); // string
